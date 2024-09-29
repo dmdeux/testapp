5bd420b609643017f69a4a30ded7037ca12a64af
Darío Martínez Martínez <151432423+dmdeux@users.noreply.github.com>
2024-09-29 22:28:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 #emoticonos en https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Testeando", page_icon=":saluting_face:", layout="wide")
-st.subheader("¡Hola, mi nombre es Darío Martínez Martínez!")
-st.title("Estudiante del CFIS en la UPC")
-st.write(
-   "Este es un texto de prueba, en el siguiente enlace tenéis [una canción chula](https://youtu.be/wM4XDbkFvhk?si=9oZM3q9hj5ynefmd)"
-)
+with st.container():
+   st.subheader("¡Hola, mi nombre es Darío Martínez Martínez!")
+   st.title("Estudiante del CFIS en la UPC")
+   st.write("Este es un texto de prueba, en el siguiente enlace tenéis [una canción chula](https://youtu.be/wM4XDbkFvhk?si=9oZM3q9hj5ynefmd)")
+   
