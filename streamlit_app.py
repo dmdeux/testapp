59f2ff0232f06102419e6bd304ea74dbd9de10ea
Darío Martínez Martínez <151432423+dmdeux@users.noreply.github.com>
2024-09-29 22:44:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,20 @@
 import streamlit as st
 #emoticonos en https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Testeando", page_icon=":saluting_face:", layout="wide")
+st.set_page_config(page_title="Testing", page_icon=":saluting_face:", layout="wide")
 with st.container():
-   st.subheader("¡Hola, mi nombre es Darío Martínez Martínez!")
-   st.title("Estudiante del CFIS en la UPC")
-   st.write("Este es un texto de prueba, en el siguiente enlace tenéis [una canción chula](https://youtu.be/wM4XDbkFvhk?si=9oZM3q9hj5ynefmd)")
-   
+   st.subheader("¡Hi, my name is Darío Martínez Martínez!")
+   st.title("Student at CFIS-UPC")
+   st.write("This is a test, meanwhile, here you have a [nice song](https://youtu.be/wM4XDbkFvhk?si=9oZM3q9hj5ynefmd)")
+
+  with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+      st.header("What I do")
+      st.write("##")
+      st.write(
+          """
+          To be honest, I still do not have any project, for the moment, making this web app is the first one. I mean, I am fine with my degree, and I do not usually have much spare time to spend in side projects. I am currently in the third year (out of five) of a double degree in Mathematics and Engineering Physics, I benefit from a scholarship from the CFIS programme at Universitat Politècnica de Catalunya.
+          I would define myself as a curious guy, who enjoys science and maths.
+          """
+      )
